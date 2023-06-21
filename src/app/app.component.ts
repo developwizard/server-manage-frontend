a31e3e3f48d61172349e3aaef99399118010c969
dewiz <churkin.it@gmail.com>
2023-06-21 23:52:05 +0300

    Implemented ping servers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,7 @@ export class AppComponent implements OnInit {
   readonly dataState = DataState;
   readonly status = Status;
   private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
   filterStatus$ = this.filterSubject.asObservable();
 
   constructor(private serverService: ServerService) {
@@ -26,6 +27,7 @@ export class AppComponent implements OnInit {
     this.appState$ = this.serverService.servers$
       .pipe(
         map(response => {
+          this.dataSubject.next(response);
           return {dataState: DataState.LOADED_STATE, appData: response}
         }),
         startWith({dataState: DataState.LOADING_STATE}),
@@ -35,4 +37,21 @@ export class AppComponent implements OnInit {
       );
   }
 
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress)
+      .pipe(
+        map(response => {
+          const index = this.dataSubject.value.data.servers.findIndex(server => server.id === response.data.server.id);
+          this.dataSubject.value.data.servers[index] = response.data.server;
+          this.filterSubject.next('');
+          return {dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}
+        }),
+        startWith({dataState: DataState.LOADED_STATE, appData: this.dataSubject.value}),
+        catchError((error: string) => {
+          this.filterSubject.next('');
+          return of({dataState: DataState.ERROR_STATE, error})
+        })
+      );
+  }
 }
